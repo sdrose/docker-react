df264a5db89ef78bbf40baa582626a8969549ced
s. david rose <drose@fedora.home.local>
2019-04-02 20:16:37 -0400

    changed app text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload -- this is a file edit ...
+            Edit <code>src/App.js</code> and save to reload.  Committed to the Feature Branch prior to merge to Master.
           </p>
           <a
             className="App-link"
